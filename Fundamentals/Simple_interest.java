9d7a5d363850b481eb3fba00c3d807adb792fea5
Rohit Singh <45510285+rohitgit1@users.noreply.github.com>
2021-04-15 13:41:23 +0530

    Add files via upload

---
+++ Fundamentals/Simple_interest.java
@@ -0,0 +1,18 @@
+package myproject;
+
+public class Simple_interest {
+
+	public static void main(String[] args) {
+		
+		int p = 1000;
+		int r = 2;
+		int t = 6;
+		
+		int si = (p*r*t)/100;
+		
+		System.out.println("SI is "+si);
+		// TODO Auto-generated method stub
+
+	}
+
+}
