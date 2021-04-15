cb1d9200630aaeb008f0277a27e340cb317c5a2a
Rohit Singh <45510285+rohitgit1@users.noreply.github.com>
2021-04-15 13:50:36 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,21 @@
+package myproject;
+
+public class ifelse {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Start");
+		int a = 17;
+		int b = 7;
+		
+		if(a < b) {
+			System.out.println("in if");
+		}else {
+			System.out.println("in else");
+		}
+		System.out.println("End");
+		// TODO Auto-generated method stub
+
+	}
+
+}
